3aac8eaae69927a0a2ab090cedd358b51691b24f
Anthony <anthony.warnault@gmail.com>
2021-06-29 21:43:08 +0200

    Add security

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
+const helmet = require('helmet');
 require('dotenv').config();
+const session = require('express-session');
+const mongoSanitize = require('express-mongo-sanitize');
+const fs = require('fs');
+const morgan = require('morgan');
+const nocache = require('nocache');
 
 const sauceRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
 
-//Connexion sur la base de données MongoDB
+// Connexion sur la base de données MongoDB
 mongoose.connect(process.env.MONGO_URI,
 { useNewUrlParser: true,
     useUnifiedTopology: true })
@@ -16,6 +22,7 @@ mongoose.connect(process.env.MONGO_URI,
 
 const app = express();
 
+// Configuration CORS
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', process.env.ORIGINE);
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -23,10 +30,38 @@ app.use((req, res, next) => {
     next();
 });
 
+// Parse les requêtes du body en format json
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// Sécurise les Headers
+app.use(helmet());
 
+// Désactive la mise en cache du navigateur
+app.use(nocache());
+
+// Sécurise les injections de charactère inapproprié
+app.use(mongoSanitize({
+    replaceWith: '_',
+}));
+
+// Enregistre toutes les demandes faites au serveur au format Apache dans le fichier access.log
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+app.use(morgan('combined', { stream: accessLogStream }))
+
+// Sécurise les cookies de la session
+app.set('trust proxy', 1)
+app.use(session({
+    secret: process.env.COOKIE_SESSION,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 30000,
+        secure: true
+    }
+}))
+
+// Routes
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,13 +1,15 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const cryptoJs = require('crypto-js');
 
 const User = require('../models/User');
 
+// Inscription utilisateur (sécurisé)
 exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
         .then(hash => {
             const user = new User({
-                email: req.body.email,
+                email: cryptoJs.HmacSHA256(req.body.email, process.env.EMAIL).toString(),
                 password: hash
             })
 
@@ -19,8 +21,10 @@ exports.signup = (req, res, next) => {
         .catch(error => res.status(500).json({ error }));
 };
 
+// Connexion au compte de l'utilisateur (sécurisé)
 exports.login = (req, res, next) => {
-    User.findOne({ email: req.body.email })
+  const findcryptedEmail = cryptoJs.HmacSHA256(req.body.email, process.env.EMAIL).toString();
+    User.findOne({ email: findcryptedEmail })
       .then(user => {
         if (!user) {
           return res.status(404).json({ error: 'Utilisateur non trouvé !' });
@@ -34,7 +38,7 @@ exports.login = (req, res, next) => {
               userId: user._id,
               token: jwt.sign(
                   { userId: user.id },
-                  'RANDOM_TOKEN_SECRET',
+                  process.env.TOKEN,
                   { expiresIn: '24h' }
               )
             });
